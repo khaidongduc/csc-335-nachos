a736de0e71f70ec62033131d3955b4d2e94dd18e
Khai Dong <dongk@union.edu>
2024-04-30 21:15:26 -0400

    starter code for BoundedBuffer

---
+++ nachos/threads/BoundedBuffer.java
@@ -0,0 +1,27 @@
+package nachos.threads;
+
+public class BoundedBuffer {
+
+    // non-default constructor with a fixed size
+    public BoundedBuffer(int maxsize) {
+
+    }
+
+    // Read a character from the buffer, blocking until there is a char
+    // in the buffer to satisfy the request. Return the char read.
+    public char read() {
+
+
+    }
+
+    // Write the given character c into the buffer, blocking until
+    // enough space is available to satisfy the request.
+    public void write(char c) {
+    }
+
+    // Prints the contents of the buffer; for debugging only
+    public void print() {
+
+    }
+
+}
