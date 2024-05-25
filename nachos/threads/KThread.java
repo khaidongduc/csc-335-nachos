d896d7026b0f26a61c86a80e332ee202e4ee7a0e
Khai Dong <dongk@union.edu>
2024-05-25 14:48:22 -0400

    fix bug where Machine.yield() is called instead of KThread.yield()

--- nachos/threads/KThread.java
+++ nachos/threads/KThread.java
@@ -295,7 +295,7 @@ public class KThread {
 	Lib.assertTrue(idleThread == null);
 	
 	idleThread = new KThread(new Runnable() {
-	    public void run() { while (true) Machine.yield(); }
+	    public void run() { while (true) KThread.yield(); }
 	});
 	idleThread.setName("idle");
 
